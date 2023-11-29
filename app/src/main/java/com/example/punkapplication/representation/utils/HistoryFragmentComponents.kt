e4be572eeb1dffb7c9b49d7c88c3c39d9786664e
extermin4tus <montarstrike777@gmail.com>
2023-11-29 22:52:18 +0300

    Implemented ui in History Screen

--- app/src/main/java/com/example/punkapplication/representation/utils/HistoryFragmentComponents.kt
+++ app/src/main/java/com/example/punkapplication/representation/utils/HistoryFragmentComponents.kt
@@ -1,10 +1,13 @@
 package com.example.punkapplication.representation.utils
 
 import android.content.Context
+import android.graphics.Rect
 import android.util.Log
+import android.view.View
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ItemDecoration
 import com.example.punkapplication.databinding.FragmentHistoryBinding
 import com.example.punkapplication.domain.model.DrinkModel
 import com.example.punkapplication.representation.ui.HistoryAdapter
@@ -16,6 +19,12 @@ fun FragmentHistoryBinding.setUpRecyclerViewWithSwipeToRemove(
 ) {
     recyclerView.adapter = adapter
     recyclerView.layoutManager = LinearLayoutManager(context)
+    recyclerView.setPadding(16, 0, 16, 16)
+    recyclerView.addItemDecoration(object : RecyclerView.ItemDecoration(){
+        override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
+            outRect.set(16, 16, 16, 16)
+        }
+    })
 
     val itemTouchHelper = ItemTouchHelper(object :
         ItemTouchHelper.SimpleCallback(ItemTouchHelper.UP or ItemTouchHelper.DOWN, ItemTouchHelper.LEFT) {
